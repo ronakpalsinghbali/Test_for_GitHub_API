8bf214700df5bad38046241c0d80936c69130aed
Ronak <ronaqpalsinghbali@gmail.com>
2024-04-03 14:10:30 +0530

    Added tf code

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_db_subnet_group" "this" {
+  name       = "test"
+  subnet_ids = var.subnet_ids
+}
+
+variable "subnet_ids" {
+  type = list[string]
+  default = []
+}
+
+output "db_subnet_group" {
+  value = aws_db_subnet_group.this.id
+}
