1e8ce3e776fe5f345f25504075da44f3510a7700
Ronak <ronaqpalsinghbali@gmail.com>
2024-05-14 17:50:01 +0530

    test

--- print-key-value.py
+++ print-key-value.py
@@ -1,4 +1,51 @@
+# import json
+
+# # Read the JSON file
+# with open('out.json', 'r') as f:
+#     data = json.load(f)
+
+# # Print the key-value pairs
+# for key, values in data.items():
+#     if isinstance(values, list):
+#         # If the value is a list, print each value separately
+#         for value in values:
+#             print(f"{key}: {value}")
+#     else:
+#         # If the value is not a list, print it directly
+#         print(f"{key}: {values}")
+
+
+# NEW
+# import json
+
+# # Read the JSON file
+# with open('out.json', 'r') as f:
+#     data = json.load(f)
+
+# # Print the key-value pairs
+# for key, values in data.items():
+#     if isinstance(values, dict) and 'value' in values:
+#         # Check if the value is a dictionary containing 'value' key
+#         value = values['value']
+#         if isinstance(value, list):
+#             # If the value is a list, print the first item
+#             print(f"{key}: {value[0]}")
+#         else:
+#             # If the value is not a list, print it directly
+#             print(f"{key}: {value}")
+
+
 import json
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+dotenv_path = os.path.join(os.path.dirname(__file__), 'env_file.env')
+load_dotenv(dotenv_path)
+
+# Load region from environment variable
+REGION = os.getenv('REGION')
+
 
 # Read the JSON file
 with open('out.json', 'r') as f:
@@ -6,16 +53,17 @@ with open('out.json', 'r') as f:
 
 # Print the key-value pairs
 for key, values in data.items():
-    if isinstance(values, list):
-        # If the value is a list, print each value separately
-        for value in values:
+    if isinstance(values, dict) and 'value' in values:
+        # Check if the value is a dictionary containing 'value' key
+        value = values['value']
+        if key == 'User_Pool_Domain':
+            # Prefix and suffix the value with the specified URL
+            value = f"https://{value}.auth.{REGION}.amazoncognito.com"
             print(f"{key}: {value}")
-    else:
-        # If the value is not a list, print it directly
-        print(f"{key}: {values}")
-
-
-
-
-
+        elif isinstance(value, list):
+            # If the value is a list, print the first item
+            print(f"{key}: {value[0]}")
+        else:
+            # If the value is not a list, print it directly
+            print(f"{key}: {value}")
 
